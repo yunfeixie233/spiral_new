2c52d74befa3e76846d8443d4e9417d49c94bb26
arda-sentient <arda@sentient.xyz>
2025-10-24 07:22:01 +0000

    scyn

--- script/type/run_tic_noeval.sh
+++ script/type/run_tic_noeval.sh
@@ -37,8 +37,8 @@ pkill -u ubuntu python
 python train_spiral.py \
     --env_ids TicTacToe-v0 \
     --use_llm_obs_wrappers False \
-    --eval_env_ids TicTacToe-v0 KuhnPoker-v1 \
-    --eval_use_llm_obs_wrappers False True \
+    --eval_env_ids TicTacToe-v0 KuhnPoker-v1 SimpleNegotiation-v1 PigDice-v1 \
+    --eval_use_llm_obs_wrappers False True True False \
     --eval_opponent_names google/gemini-2.0-flash-lite-001 \
     --eval_split all \
     --gamma 1 \
@@ -80,4 +80,6 @@ python train_spiral.py \
     --wb-run-name $SCRIPT_NAME \
     --wb-project spiral \
     --save-ckpt \
-    --debug 
+    --debug \
+    --skip_game_eval \
+    --skip_dataset_eval
